a126f69cd6f588b0719dad05aebfa6e3e8507db7
DrAtomic <leo.ward.garcia@gmail.com>
2020-11-30 18:21:04 -0800

    simplified imageparse and updated readme

--- imageparse.py
+++ imageparse.py
@@ -22,146 +22,47 @@ def display_image(img):
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-
 #display_image(img)
 
-def process_image(img, skip_dilate=False):
-    """takes an image and processes it
-    
-    Args:
-       img: image to be processed
-    
-    Returns:
-        processed image
-    
-    """
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    blur = cv2.GaussianBlur(gray,(23,23), 0)
-    thresh = cv2.adaptiveThreshold(blur,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,11,2)
-    thresh = cv2.bitwise_not(thresh,thresh)
-    
-    
-    if not skip_dilate:
-        kernel = np.array([[0.0, 1.0, 0.0],
-                           [1.0, 1.0, 1.0],
-                           [0.0, 1.0, 0.0]],np.uint8)
-        
-        thresh = cv2.dilate(thresh,kernel)
-        
-    return thresh
-
-#display_image(process_image(img))
-
-def find_corners(image):
-    """finds the conrers of the grid
-    
-    Args:
-       image: processed image
-    
-    Returns:
-        corner coordinates
-    
-    """
-    # TODO: fix this. it rotates the image
-    ext_contours = cv2.findContours(image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    ext_contours = ext_contours[0] if len(ext_contours) == 2 else ext_contours[1]
-    ext_contours = sorted(ext_contours, key=cv2.contourArea, reverse=True)
-
-   
-    for c in ext_contours:
-        peri = cv2.arcLength(c, True)
-        approx = cv2.approxPolyDP(c, 0.015 * peri, True)
-        if len(approx) == 4:
-            return approx
-
-#t = find_corners(process_image(img))
-#print(t)
-
-def sort_corners(corners):
-    """sorts corners clockwise
-    
-    Args:
-       corners: unsorted corners
-    
-    Returns:
-        sorted corners
-    
-    """
-    corners = [(corner[0][0], corner[0][1]) for corner in corners]
-    top_right, top_left, bottom_left, bottom_right = corners[0], corners[1], corners[2],corners[3] #[x_coord, y_coord]
-    return  top_left, top_right,  bottom_right, bottom_left
-
-
-
-def crop_image(image, corners):
-    """crops an image at the grid
-    
-    Args:
-       image: original image
-       corners: unsorted corners
-    
-    Returns:
-        cropped image that you can see with display_image()
-    
-    """
-    orderd_corners = sort_corners(corners)
-    top_left, top_right, bottom_right, bottom_left = orderd_corners
-    
-    
-    #determine the new croped image
-    ## first find the greatest width between top or bottom
-    bottom_width = np.sqrt(((bottom_right[0] - bottom_left[0]) ** 2) + ((bottom_right[1] - bottom_left[1]) ** 2))
-    top_width = np.sqrt(((top_right[0] - top_left[0]) ** 2) + ((top_right[1] - top_left[1]) ** 2))
-    width = max(int(bottom_width),int(top_width))
-    
-    ## find greatest height
-    right_height = np.sqrt(((top_right[0] - bottom_right[0]) ** 2) + ((top_right[1] - bottom_right[1]) ** 2))
-    left_height = np.sqrt(((top_left[0] - bottom_left[0]) ** 2) + ((top_left[1] - bottom_left[1]) ** 2))
-    height = max(int(right_height), int(left_height))
-    
-    dimensions = np.array([[0, 0],
-                           [width - 1, 0],
-                           [width - 1, height - 1],
-                           [0, height - 1]],
-                          dtype="float32")
-    
-    orderd_corners = np.array(orderd_corners, dtype="float32")
-    grid = cv2.getPerspectiveTransform(orderd_corners,dimensions)
-    
-    return cv2.rotate(cv2.warpPerspective(image, grid, (width, height)),cv2.cv2.ROTATE_90_COUNTERCLOCKWISE)
-
-#display_image(crop_image(img,find_corners(process_image(img))))
 
 
 def analyze_cells(img,pwd):
-    """takes a croped image and analyze_cells the cells, saves pixels of cells into a row in a csv
+    """takes an image and analyze_cells the cells, saves the cells in a folder
     
     Args:
        img: croped image
        pwd: path to working directory
     
     """
-   
+    MAX_CELLS = 100 #number of cells
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    blurred = cv2.GaussianBlur(gray, (33,33), 0)
-    thresh = cv2.threshold(blurred, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+    blur = cv2.GaussianBlur(gray, (33,33), 0)
+    thresh = cv2.threshold(blur,127,255,cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
     
+    cnts, heirarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
     
-    thresh = 255 - thresh
-    
-    cnts = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-    cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+    if (len (heirarchy[0]) != MAX_CELLS + 1):
+        print("Warning: too many contours, increasing blur")
+        print(str(len(heirarchy[0])) + " contours")
+        blur = cv2.GaussianBlur(gray, (93,93), 0)
+        thresh = cv2.threshold(blur,127,255,cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+        cnts, heirarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+        if (len(heirarchy[0]) != MAX_CELLS + 1):
+            print("ERROR: could not fix, exiting")
+            exit(1)
+
+        
     count = 0
-    for cnt in cnts:
-        x,y,w,h = cv2.boundingRect(cnt)
-        cropped = img[y:y+h,x:x+w]
-        try:
-            np.os.remove(pwd + 'cell_images' + str(count) + 'jpg')
-        except:
-            pass
-        cv2.imwrite(pwd+'/cell_images/cell' + str(count) + '.jpg',cropped)
-        count +=1
-   
+    for i in range(len(cnts)):
+        if (heirarchy[0][i][3] != -1):
+            x,y,w,h = cv2.boundingRect(cnts[i])
+            cropped = img[y:y+h, x:x+w]
+            try:
+                os.remove(pwd + '/cell_images/cell' + str(count) + '.jpg')
+            except:
+                pass
+            cv2.imwrite(pwd+'/cell_images/cell' + str(count) + '.jpg',cropped)
+            count +=1
 
 def parse_grid(path_to_image,pwd):
     """takes a path to an image writes all the cells in a folder called cell_images
@@ -171,9 +72,8 @@ def parse_grid(path_to_image,pwd):
 
 
     """
-    img = cv2.imread(path_to_image)
-    
-    analyze_cells(crop_image(img,find_corners(process_image(img))),pwd)
+    img = cv2.imread(path_to_image)  
+    analyze_cells(img,pwd)
 
 
-parse_grid('image0.jpg',os.getcwd())
+#parse_grid("image0.jpg",os.getcwd())
